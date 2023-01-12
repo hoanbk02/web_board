4a0702ee867d16e2c96dfd876ecb5113289a4bea
hoan.phamphu <hoan.pham@genefriendway.com>
2023-01-12 11:43:10 +0700

    lesson 15: reuse template, url, basic form

--- web_board/urls.py
+++ web_board/urls.py
@@ -23,7 +23,9 @@ from boards import views
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
-    url(r'^$', views.home, name='home'),
     path('admin/', admin.site.urls),
 
+    url(r'^$', views.home, name='home'),
+    url(r'^board/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^board/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
