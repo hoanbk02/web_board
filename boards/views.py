16c7cf699a2bb79ba1bfe8fd3a207a5af4f81acc
hoan.phamphu <hoan.pham@genefriendway.com>
2023-01-12 17:03:26 +0700

    lesson 16: reuse template, widget_tweaks, advanced form

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 
+from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
 
@@ -18,24 +19,23 @@ def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
 
     if request.method == 'POST':
-        request_data = request.POST
-        subject = request_data['subject']
-        message = request_data['message']
-
         user = User.objects.first()
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user,
-        )
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user,
-            updated_by=user
-        )
+            topic.board = board
+            topic.starter = user
+            topic.save()
 
-        return redirect('board_topics', pk=board.pk)
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user,
+                updated_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
 
-    return render(request, 'new_topic.html', {'board': board})
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
